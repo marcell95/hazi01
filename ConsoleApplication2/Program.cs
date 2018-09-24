ffe103ac263e2cfcfa7ba11dc26deaa15cc9a888
marcell <marcellszasz17@gmail.com>
2018-09-24 15:44:04 +0200

    1-2. félkész

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -10,7 +10,39 @@ namespace ConsoleApplication2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Csak azért is változtatás!");
+
+            //Console.WriteLine("(Dont give up , Skeleton!)" +
+            //    "\nAdd meg a bekérendő számok mennyiségét! (most)");
+            //String atmero = Console.ReadLine();
+
+            Console.WriteLine("Add meg a számot!");
+            int nSzam = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(nSzam);
+            Console.ReadLine();
+            int[] t = new int[nSzam];
+
+            for (int i = 0; i < nSzam; i++)
+            {
+                Console.WriteLine("Add meg a "+ (i + 1) + ". számot!");
+                t[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+            Console.ReadLine();
+
+
+            //atmero = double.Parse(Console.ReadLine());
+            //double asdd;
+
+            //if (!double.TryParse(Console.ReadLine(), out asdd))
+            //{
+            //    Console.Clear();
+            //    Console.WriteLine("Hibás adat! Kérem adja meg újra", 50, 2, ConsoleColor.Black, ConsoleColor.Red);
+            //}
+            //else {
+            //    Console.WriteLine("nyasgem");
+
+
         }
+        
     }
 }
